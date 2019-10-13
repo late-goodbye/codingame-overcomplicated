b54f2b663e1544bbe4a8dd5fa44a51eeb9102cc6
Constantine <cmen.itis@gmail.com>
2019-10-13 10:16:31 +0300

    add decode fucntion description

--- easy/1_disordered_first_contact/disordered_first_contact.py
+++ easy/1_disordered_first_contact/disordered_first_contact.py
@@ -35,6 +35,37 @@ class Coder(object):
         message.text = encoded_message
 
     def decode(self, message):
+        """
+        Decodes message once.
+
+        The first step is to determine how many bits the message contains and lengths of the bits.
+        The bits lengths in common are 1, 2, 3, ..., k, k + 1, ... .
+        We know only the message's total length, it equals 1 + 2 + 3 + ... + k + k'
+        k' is one of the next values: [0..k+1].
+        Sum of all bits except the last one is 1 + 2 + 3 + ... + k = (1 + k) / 2 * (n - 1) = l.
+        Total length is L = l + k', hence k' = L - l.
+
+        L is already known.
+        n - 1 = k => (1 + k) / 2 * k = l
+        k^2 + k - 2l = 0
+
+        if k' = 0 then L = l, n = k
+        if k' = k + 1 then L = l, n = k + 1
+
+        Let's find k for these cases:
+
+        k^2 + k - 2L = 0
+        ...
+
+
+
+
+        l + k' = (1 + k) / 2 * n
+        2(l + k') - (1 + k) n = 0
+
+
+
+        """
         decoded_message = ''
         self.logger.info('Decoded message "{}" to "{}"'.format(message, decoded_message))
 
