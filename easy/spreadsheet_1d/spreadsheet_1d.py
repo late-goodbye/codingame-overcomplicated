da5bbe15678709d72737b1b7469d347ca6ebdf29
Constantine <cmen.itis@gmail.com>
2019-10-25 21:51:20 +0300

    finish spreadsheet_1d

--- easy/spreadsheet_1d/spreadsheet_1d.py
+++ easy/spreadsheet_1d/spreadsheet_1d.py
@@ -1,3 +1,4 @@
+import re
 from abc import ABC, abstractmethod
 from enum import Enum
 
@@ -22,6 +23,10 @@ class Spreadsheet:
     def __init__(self, num_cells):
         self.cells = [Cell() for _ in range(num_cells)]
 
+    def apply_formulas(self, formulas):
+        for formulae, cell in zip(formulas.split('\n'), self.cells):
+            cell.formulae = formulae
+
     @cached
     def calculate_value(self, cell_index):
 
@@ -29,6 +34,18 @@ class Spreadsheet:
         operation, arg1, arg2 = self.parse_formulae(formulae)
 
         return operation(arg1, arg2)
+
+    def parse_formulae(self, formulae):
+        operation, arg1, arg2 = formulae.split()
+        arg1, arg2 = map(self.parse_arg, (arg1, arg2))
+        operation = Operations.__members__[operation]
+        return operation, arg1, arg2
+
+    def parse_arg(self, arg):
+        m = re.search(r'\$(?P<cell_index>[0-9]+)', arg)
+        if m:
+            return self.calculate_value(int(m.group('cell_index')))
+        return int(arg) if arg.isdigit else None
 
 
 class Operation(ABC):
@@ -58,4 +75,10 @@ class Multiplication(Operation):
 
 
 class Operations(Enum):
+    VALUE = SetValue
+    ADD = Addition
+    SUB = Subtraction
+    MULT = Multiplication
+
+if __name__ == '__main__':
     pass
