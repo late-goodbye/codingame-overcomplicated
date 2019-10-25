d9cb6a3012b4427e61f0b6def7f78242cbb91fd3
k.menshenin <konstantin.menshenin@tionix.ru>
2019-10-25 16:47:02 +0300

    update spreadsheet_1d

--- easy/spreadsheet_1d/spreadsheet_1d.py
+++ easy/spreadsheet_1d/spreadsheet_1d.py
@@ -1,93 +1,61 @@
-import re
 from abc import ABC, abstractmethod
 from enum import Enum
 
 
-class Cell(object):
+def cached(func):
+    cache = {}
 
-    def __init__(self, spreadsheet):
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+
+    return wrapper
+
+
+class Cell:
+    def __init__(self):
         self.formulae = None
-        self.calculated_value = None
-        self.spreadsheet = spreadsheet
-
-    @property
-    def value(self):
-        return self.calculated_value or self.calculate_value()
-
-    def calculate_value(self):
-        op, arg1, arg2 = map(self.resolve, self.formulae.split())
-        return op.perform(arg1, arg2)
-
-    def resolve(self, param):
-        return self.resolve_operator(param) or self.resolve_arg(param) or None
-
-    def resolve_arg(self, arg):
-        m = re.search(r'\$(?P<index>[0-9]+)', arg)
-        if m:
-            return self.spreadsheet.get_cell(int(m.group('index'))).value
-        elif arg.isdigit():
-            return int(arg)
-
-    @staticmethod
-    def resolve_operator(operator):
-        return Operations.get[operator].value or None
 
 
-class Spreadsheet(object):
-
+class Spreadsheet:
     def __init__(self, num_cells):
-        self.cells = [Cell(self) for _ in num_cells]
-        self.cursor = 0
+        self.cells = [Cell() for _ in range(num_cells)]
 
-    def get_cell(self, index):
-        return self.cells[index]
+    @cached
+    def calculate_value(self, cell_index):
 
-    def set_formulae(self, formulae, cursor=None):
-        cursor = cursor or self.cursor
-        self.get_cell(cursor).formulae = formulae
+        formulae = self.cells[cell_index].formulae
+        operation, arg1, arg2 = self.parse_formulae(formulae)
 
-    def calculate_cells(self):
-        #TODO finish later
-        pass
+        return operation(arg1, arg2)
+
 
 class Operation(ABC):
-
-    @classmethod
     @abstractmethod
-    def perform(cls, arg1: int, arg2: int):
+    def perform(self, arg1, arg2):
         pass
 
 
 class SetValue(Operation):
-
-    @classmethod
-    def perform(cls, arg1: int, arg2: int):
+    def perform(self, arg1, arg2):
         return arg1
 
 
 class Addition(Operation):
-
-    @classmethod
-    def perform(cls, arg1: int, arg2: int):
+    def perform(self, arg1, arg2):
         return arg1 + arg2
 
 
 class Subtraction(Operation):
-
-    @classmethod
-    def perform(cls, arg1: int, arg2: int):
+    def perform(self, arg1, arg2):
         return arg1 - arg2
 
 
 class Multiplication(Operation):
-
-    @classmethod
-    def perform(cls, arg1: int, arg2: int):
+    def perform(self, arg1, arg2):
         return arg1 * arg2
 
 
 class Operations(Enum):
-    VALUE = SetValue
-    ADD = Addition
-    SUB = Subtraction
-    MULT = Multiplication
+    pass
